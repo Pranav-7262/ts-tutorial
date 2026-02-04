2ff23ef56de6a883ae2b284152e2462e375dea82
Pranav-7262 <varpepranav8@gmail.com>
2026-02-04 21:32:09 +0530

    implements functions in ts

---
+++ src/functionsTS.ts
@@ -0,0 +1,30 @@
+// function makeChai(type : string , cups : number) {
+//     console.log(`Making ${cups} cups of ${type}`);
+// }
+// makeChai("masala" , 3)
+
+// const getDoubleOfNum = (num : number):number => {
+//     return num*2
+// }
+// console.log(getDoubleOfNum(12));
+
+// function logging():void {
+//     console.log("logging..");
+// }
+
+// function orderChai(type?: string) { // optional value
+//   console.log("jekne");
+// }
+// orderChai()
+
+// function orderChai(type: string = "Ginger Tea") { // default value
+//     return `type is${type}`
+// }
+
+// function createChai(order: {
+//   type: string;
+//   sugar: number;
+//   size: "small" | "large";
+// }): number {
+//   return 3;
+// }
